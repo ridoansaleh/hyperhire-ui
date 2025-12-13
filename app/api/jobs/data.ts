3692738ba468708a05476bd254a542574bcd1298
Ridoan <ridoansalehnasution98@gmail.com>
2025-12-13 21:58:00 +0700

    chore(images): renamed files according to their purpose

--- app/api/jobs/data.ts
+++ app/api/jobs/data.ts
@@ -2,27 +2,27 @@ import type { Job } from "@/types/Job";
 
 export const jobs: Job[] = [
   {
-    logo: "/images/feature1.png",
+    logo: "/images/job1.png",
     label: "해외 마케팅",
     alt: "해외 마케팅",
   },
   {
-    logo: "/images/feature2.png",
+    logo: "/images/job2.png",
     label: "퍼블리셔",
     alt: "퍼블리셔",
   },
   {
-    logo: "/images/feature3.png",
+    logo: "/images/job3.png",
     label: "캐드원(제도사)",
     alt: "캐드원(제도사)",
   },
   {
-    logo: "/images/feature4.png",
+    logo: "/images/job4.png",
     label: "해외 세일즈",
     alt: "해외 세일즈",
   },
   {
-    logo: "/images/feature5.png",
+    logo: "/images/job5.png",
     label: "해외 CS",
     alt: "해외 CS",
   },
