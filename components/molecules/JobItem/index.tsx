c44e1b6cd27789b4db0ef307c0488e2619883000
Ridoan <ridoansalehnasution98@gmail.com>
2025-12-18 19:50:16 +0700

    fix(style): handle dynamic width without Tailwind

--- components/molecules/JobItem/index.tsx
+++ components/molecules/JobItem/index.tsx
@@ -6,15 +6,11 @@ interface JobItemProps {
   itemWidth: number;
 }
 
-export default function JobItem({
-  alt,
-  label,
-  logo,
-  itemWidth,
-}: JobItemProps) {
+export default function JobItem({ alt, label, logo, itemWidth }: JobItemProps) {
   return (
     <div
-      className={`shrink-0 w-[${itemWidth}px] flex items-center gap-[12px] bg-[#FFFFFF33] p-[16px] rounded-[12px]`}
+      className="shrink-0 flex items-center gap-[12px] bg-[#FFFFFF33] p-[16px] rounded-[12px]"
+      style={{ width: `${itemWidth}px` }}
     >
       <Image src={logo} alt={alt} width={48} height={48} />
       <label className="text-xl font-black">{label}</label>
